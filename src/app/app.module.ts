11002c22eab36de60a500519fdd3857b8586387b
Mattia Rebesan <mattiarebesan@gmail.com>
2021-03-12 09:01:08 +0100

    Clean Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,51 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule, DesktopGuard, MobileGuard } from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { WipComponent } from './wip/wip.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { MobileBodyComponent } from './mobile-body/mobile-body.component';
 import { HomeComponent } from './home/home.component';
-import { MobileHomeComponent } from './mobile-home/mobile-home.component';
-import { MobileWipComponent } from './mobile-wip/mobile-wip.component';
-import { MobileHeaderComponent } from './mobile-header/mobile-header.component';
-import { MobileContactsComponent } from './mobile-contacts/mobile-contacts.component';
 import { CollaborationsComponent } from './collaborations/collaborations.component';
-import { MobileCollaborationsComponent } from './mobile-collaborations/mobile-collaborations.component';
 import { RenderComponent } from './render/render.component';
-import { MobileRenderComponent } from './mobile-render/mobile-render.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    HeaderComponent,
     BodyComponent,
     WipComponent,
     ContactsComponent,
-    MobileBodyComponent,
     HomeComponent,
-    MobileHomeComponent,
-    MobileWipComponent,
-    MobileHeaderComponent,
-    MobileContactsComponent,
     CollaborationsComponent,
-    MobileCollaborationsComponent,
     RenderComponent,
-    MobileRenderComponent,
-    HomepageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [ DesktopGuard, MobileGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,72 +1,145 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
 
-import { fade } from '../../animations';
-
-import { ImageTemplate } from './home.templates';
-import * as enumerations from './home.templates';
+class Section {
+  imagePath: string;
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  animations: [fade]
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit, OnInit, OnDestroy {
 
-  selectedTemplate: ImageTemplate;
-  currentTemplateIndex = 0;
-  templatesNumber: number;
+  sections: Section[] = [
+    {
+      imagePath: 'assets/images/img_1.jpg',
+      title: 'Costruisci il tuo sogno',
+      description: 'Ogni persona ha il suo sogno, piccolo o grande che sia. ' +
+        'Dalle prime fasi della progettazione fino alla sua realizzazione ci sarà sempre qualcuno ad accompagnarti.'
+    },
+    {
+      imagePath: 'assets/images/img_2.jpg',
+      title: 'Reali fin da subito',
+      description: 'Non hai le idee chiare? ' +
+        'Con i più sofisticati software di progettazione tridimensionale i tuoi pensieri prenderanno vita davanti ai tuoi occhi.'
+    },
+    {
+      imagePath: 'assets/images/img_3.jpg',
+      title: 'Il sostegno che cerchi',
+      description: 'Progettare costruzioni in ferro non è mai stato così semplice e veloce. Dai il peso giusto ai tuoi progetti.'
+    }
+  ];
 
-  intervalId: any;
-
-  swapImg1: string;
-  swapImg2: string;
+  timelines = [];
+  scrollTriggers = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    this.selectedTemplate = enumerations.templates[this.currentTemplateIndex];
-    this.swapImg1 = this.selectedTemplate.imagePath;
-    this.templatesNumber = enumerations.templates.length;
-
-    this.startInterval();
-
+    gsap.delayedCall(1, () => ScrollTrigger.refresh());
   }
 
-  private startInterval() {
-    this.intervalId = setInterval(() => {
-      this.rotateTemplate();
-    }, 10 * 1000);
-  }
-  private restartInterval() {
-    clearInterval(this.intervalId);
-    this.startInterval();
+
+  ngOnDestroy(): void {
+    this.scrollTriggers.forEach(t => t.kill());
+    this.timelines.forEach(t => {
+      if (t.scrollTrigger) {
+        t.scrollTrigger.kill();
+      }
+      t.kill();
+    });
   }
 
-  private rotateTemplate(): void {
-    if (this.currentTemplateIndex < this.templatesNumber - 1) {
-      this.currentTemplateIndex++;
-    } else {
-      this.currentTemplateIndex = 0;
+  ngAfterViewInit(): void {
+
+    gsap.utils.toArray('.ir-overlay').forEach((panel: Element) => {
+      const trig = ScrollTrigger.create({
+        trigger: panel,
+        scroller: 'section',
+        start: 'top top',
+        pin: true,
+        pinSpacing: false
+      });
+
+      this.scrollTriggers.push(trig);
+    });
+
+    gsap.utils.toArray('.ir-overlay-info').forEach((elem: Element) => {
+      this.hide(elem);
+
+      const trig = ScrollTrigger.create({
+        trigger: elem,
+        scroller: 'section',
+        onEnter: () => { this.animateFrom(elem, 1) },
+        onEnterBack: () => { this.animateFrom(elem, -1) },
+        onLeave: () => { this.hide(elem) }
+      });
+
+      this.scrollTriggers.push(trig);
+    });
+
+    if (window.innerWidth > 600) {
+      const tl = gsap.to('img.ir-overlay-logo-big',
+        {
+          opacity: 0,
+          duration: 1,
+          ease: 'power1.inOut',
+          scrollTrigger: {
+            trigger: 'img.ir-overlay-logo-big',
+            scroller: 'section',
+            start: '-20px top',
+            end: '+=400',
+            pin: true,
+            pinSpacing: false,
+            scrub: true
+          }
+        });
+
+      this.timelines.push(tl);
     }
-    this.loadTemplate();
   }
 
-  public switchTemplate(index: number): void {
-    this.currentTemplateIndex = index;
-    this.loadTemplate();
-    this.restartInterval();
-  }
+  animateFrom(elem: Element, direction: number): void {
+    if (window.innerWidth > 600) {
+      direction = direction | 1;
 
-  private loadTemplate() {
-    this.selectedTemplate = enumerations.templates[this.currentTemplateIndex];
-    if (this.swapImg1) {
-      this.swapImg1 = null;
-      this.swapImg2 = this.selectedTemplate.imagePath;
+      var x = 0, y = 0;
+      if (elem.classList.contains('ir-overlay-even')) {
+        x = 200;
+        y = 0;
+      } else if (elem.classList.contains('ir-overlay-odd')) {
+        x = -200;
+        y = 0;
+      }
+
+      gsap.fromTo(elem, { x: x, y: y, autoAlpha: 0 }, {
+        duration: 1.5,
+        x: 0,
+        y: 0,
+        delay: .5,
+        autoAlpha: 1,
+        ease: 'expo',
+        overwrite: 'auto'
+      });
     } else {
-      this.swapImg2 = null;
-      this.swapImg1 = this.selectedTemplate.imagePath;
+      gsap.fromTo(elem, { autoAlpha: 0 }, {
+        duration: 1.5,
+        delay: .5,
+        autoAlpha: 1,
+        ease: 'expo',
+        overwrite: 'auto'
+      });
     }
   }
+
+  hide(elem: Element): void {
+    gsap.to(elem, { autoAlpha: 0, duration: .8 });
+  }
+
 
 }
+
